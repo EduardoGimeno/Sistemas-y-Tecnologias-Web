3971a5bfd86bf34833f25a57c94d75acd2b185e9
Joaquin <joaquinpuyuelolardies@gmail.com>
2020-05-22 11:35:35 +0200

    Creado servicio de registro de usuarios.

--- STWEB-Angular/src/app/registry/registry.component.ts
+++ STWEB-Angular/src/app/registry/registry.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserApp } from "../entities/usuario";
+import { UserService } from "../services/user-service.service";
 
 @Component({
   selector: 'app-registry',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registry.component.css']
 })
 export class RegistryComponent implements OnInit {
+  private newUser: UserApp = null;
 
-  constructor() { }
+  constructor(public userService: UserService) { }
 
   ngOnInit(): void {
+  }
+
+  registerUser(name: string, apellidos: string, nacimiento: Date, telefono: string, pais: string,
+    provincia: string, email: string, password1: string, password2: string) {
+    this.newUser = {
+          id: 'id',
+          activo: false,
+          baneado: false,
+          fechaNacimiento: nacimiento,
+          pais: pais,
+          email: email,
+          contrasena: password1,
+          provincia: provincia,
+          apellidos: apellidos,
+          telefono: telefono,
+          nombre: name
+        };
+        this.userService.register(this.newUser);
   }
 
 }
--- STWEB-Angular/src/app/services/user-service.service.ts
+++ STWEB-Angular/src/app/services/user-service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
+import { UserApp } from "../entities/usuario";
 
 @Injectable({
   providedIn: 'root'
@@ -14,27 +15,20 @@ export class UserService {
     return this.http.get( 'localhost:3000/logIn', {params: params});
   }
 
-  public register(nombre: string, apellidos: string, fechaNacimiento: Date, email: string, contrasena: string,
-                    telefono: string, pais: string, provincia: string, activo: boolean, baneado: boolean) {
-    /* const dateUser: Date = new Date();
-    let params = new HttpParams()
-          .set("nombre", nombre).set("apellidos", apellidos).set(dateUser, fechaNacimiento)
-          .set("email", email).set("contrasena", contrasena).set("telefono", telefono).set("pais", pais)
-          .set("provincia", provincia).set("activo", activo).set("baneado", baneado);
-        return this.http.post( 'localhost:8080/logIn', {params: params});
-        */
+  public register(user: UserApp) {
+    return this.http.post( 'localhost:3000/register', user);
   }
 
-  public modifyPassword() {
-
+  public modifyPassword(id: number, password: string) {
+    return this.http.patch( 'localhost:3000/modifyPassword/' + id, password);
   }
 
   public getUsers(){
-    return this.http.get('localhost:8080/getUsers');
+    return this.http.get('localhost:3000/getUsers');
   }
 
-  public deleteUser() {
-
+  public deleteUser(id: number) {
+    return this.http.delete('localhost:3000/delete/' + id);
   }
 
 }
