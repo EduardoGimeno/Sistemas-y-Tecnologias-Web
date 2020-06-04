da9d1b370e3f01bbfb484359c724f08ccaefa592
Joaquin <joaquinpuyuelolardies@gmail.com>
2020-06-04 17:18:41 +0200

    Login con cuenta de google actualizado.

--- STWEB-Angular/src/app/login/login.component.ts
+++ STWEB-Angular/src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { CurrentUserService } from "../current-user.service";
 
 import { AuthService } from "angularx-social-login";
+import { SocialUser } from "angularx-social-login";
 import { FacebookLoginProvider, GoogleLoginProvider } from "angularx-social-login";
 
 @Component({
@@ -12,13 +13,28 @@ import { FacebookLoginProvider, GoogleLoginProvider } from "angularx-social-logi
 })
 export class LoginComponent implements OnInit {
 
+  public user: SocialUser;
+  private loggedIn: boolean;
+
+
   constructor(public router: Router, public currentUser: CurrentUserService, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authService.authState.subscribe((user) => {
+          this.user = user;
+          this.loggedIn = (user != null);
+        });
+
   }
+
 
   signInWithGoogle(): void {
       this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    var id_token = googleUser.getAuthResponse().id_token; //Token para el backend
   }
 
   signOut(): void {
