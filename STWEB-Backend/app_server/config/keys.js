7c7f07871c5a84df0384a4a10a3abaa4ebdc1e13
Edu7216 <721615@unizar.es>
2020-06-11 12:46:25 +0200

    Enviar email

--- STWEB-Backend/app_server/config/keys.js
+++ STWEB-Backend/app_server/config/keys.js
@@ -1,9 +1,5 @@
-//var clienteID = process.env.CLIENTE_ID
-//var secretID = process.env.SECRET_ID
-//var jwtClave = process.env.JWT_CLAVE
-
-var clienteID = "976260986991-vqh5r1jreamgnt6k6m10a6ga7hkqieqj.apps.googleusercontent.com";
-var secretID = "Ox15_KKXc6H3SiPqNR8zjZHR";
-var jwtClave = "claveDescubreAragon";
+var clienteID = process.env.CLIENTE_ID
+var secretID = process.env.SECRET_ID
+var jwtClave = process.env.JWT_CLAVE
 
 module.exports = {clienteID , secretID, jwtClave}
