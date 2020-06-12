bee5bfcbe9ec00b896e1c372f4e1de6c9200cce8
Jorge <jorge97fernandez@gmail.com>
2020-06-12 17:29:49 +0200

    Post request

--- STWEB-Backend/app_server/routes/media.js
+++ STWEB-Backend/app_server/routes/media.js
@@ -2,6 +2,6 @@ var express = require('express');
 var mediaController = require('../controllers/mediaController');
 var router = express.Router();
 
-router.get('/csv', mediaController.getCSV);
+router.post('/csv', mediaController.getCSV);
 
 module.exports = router;
