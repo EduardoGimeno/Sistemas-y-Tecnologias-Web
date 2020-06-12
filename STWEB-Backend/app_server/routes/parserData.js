866e3c7acedd000838dbe59aa4ef48a32647f0d1
Gonzalo Berne Melguizo <gera1397@gmail.com>
2020-06-12 02:44:51 +0200

    ParserData

--- STWEB-Backend/app_server/routes/parserData.js
+++ STWEB-Backend/app_server/routes/parserData.js
@@ -3,5 +3,6 @@ var parserDataController = require('../controllers/parserDataController');
 var router = express.Router();
 
 router.get('/guide', parserDataController.guias);
+router.get('/apartments', parserDataController.apartamentos);
 
 module.exports = router;
